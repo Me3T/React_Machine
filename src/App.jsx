335669a323d7c1804b7bf91a345eea2adeaa5b7c
Me3T <rai.manmeet1998@gmail.com>
2024-12-08 18:45:28 +0530

    Todo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Todo from "./TODO";
+
+function App() {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TODO/index.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./styles.css";
+const Todo = () => {
+  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = () => {
+    if (input.trim() === "") return;
+
+    const newTask = {
+      id: Date.now(),
+      text: input,
+    };
+    setTodo([...todo, newTask]);
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    setTodo(todo.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={input}
+        placeholder="Enter a task"
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+
+      <ul>
+        {todo.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <span>{todo.text}</span>
+              <button onClick={() => deleteTodo(todo.id)}> Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Todo;
