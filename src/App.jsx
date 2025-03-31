7ab10023b4e6359395859c0fb53e2657ae7f3cac
Me3T <rai.manmeet1998@gmail.com>
2025-03-31 21:16:06 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import DataFetch from "./data-fetch";
 import Todo from "./TODO";
 import Pagination from "./Pagination";
 import Tabform from "./Tabform";
+import Otp from "./otp";
 
 function App() {
   return (
     <div>
       {/* <Todo /> */}
-      <DataFetch />
+      {/* <DataFetch /> */}
       {/* <Pagination /> */}
       {/* <Tabform /> */}
+      <Otp />
     </div>
   );
 }
